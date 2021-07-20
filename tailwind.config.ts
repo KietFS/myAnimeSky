d98e17cd58b7023338398f286a8374d441af7881
Kiet_web_dev <72301026+KietDarealist@users.noreply.github.com>
2021-07-20 19:25:49 +0700

    aasa

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,5 @@
 // tailwind.config.js
 module.exports = {
-  purge: {
-    mode: 'all',
-    preserveHtmlElements: false,
-    content: [
-      './src/**/*.js',
-      './node_modules/flatpickr/**/*.js',
-    ],
-  },
+  purge: false,
   // ...
 }
